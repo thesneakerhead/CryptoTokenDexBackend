1b158127305f97be2bf232a1a12cf1155b4e6e91
Jacob Law <jacoblawzhen@gmail.com>
2022-10-10 14:43:06 +0800

    some changes

--- brownie/react/client/src/getWeb3.js
+++ brownie/react/client/src/getWeb3.js
@@ -5,12 +5,14 @@ export const getWeb3 = async () => {
 
     const ethereum = await getEthereum()
     let web3
-
+    // if detect metamask chain provider
     if (ethereum) {
         web3 = new Web3(ethereum)
     } else if (window.web3) {
         web3 = window.web3
-    } else {
+    }
+    // else use localhost 
+    else {
         const provider = new Web3.providers.HttpProvider(
             "http://127.0.0.1:8545"
         );
--- brownie/react/scripts/deploy.py
+++ brownie/react/scripts/deploy.py
@@ -10,13 +10,17 @@ def main():
     if network.show_active()=='development':
         # add these accounts to metamask by importing private key
         owner = accounts[0]
-        SolidityStorage.deploy({'from':accounts[0],
+        simple_storage = SolidityStorage.deploy({'from':accounts[0],
                                 'gas_price':gas_strategy})
-        VyperStorage.deploy({'from':accounts[0],
-                            'gas_price':gas_strategy})
-
+        print(simple_storage.get())
+        transaction = simple_storage.set(15,{'from':accounts[0]})
+        # VyperStorage.deploy({'from':accounts[0],
+        #                     'gas_price':gas_strategy})
+        transaction.wait(1)
+        print(simple_storage.get())
+        
     elif network.show_active() == 'kovan':
         # add these accounts to metamask by importing private key
         owner = accounts.load("main")
         SolidityStorage.deploy({'from':owner})
-        VyperStorage.deploy({'from':owner})
+        # VyperStorage.deploy({'from':owner})
